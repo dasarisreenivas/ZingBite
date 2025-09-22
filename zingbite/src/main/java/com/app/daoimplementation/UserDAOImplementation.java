d56416fecbae3074002ade10aa4dcff63e8e87ff
Sreenivasulu <sreenivasdasariv2004@gmail.com>
2025-09-22 08:28:04 +0530

    added DBUtil

---
+++ zingbite/src/main/java/com/app/daoimplementation/UserDAOImplementation.java
@@ -0,0 +1,38 @@
+package com.app.daoimplementation;
+
+import com.app.DAO.UserDAO;
+import com.app.model.User;
+
+public class UserDAOImplementation implements UserDAO {
+
+	@Override
+	public int addUser(User u) {
+		
+		return 0;
+	}
+
+	@Override
+	public void getAllUsers() {
+		
+		
+	}
+
+	@Override
+	public void getUser(String email) {
+		
+		
+	}
+
+	@Override
+	public void updateUser(User u) {
+		
+		
+	}
+
+	@Override
+	public void deleteUser(int userID) {
+		
+		
+	}
+
+}
---
+++ zingbite/src/main/java/com/app/utils/DBUtils.java
@@ -0,0 +1,24 @@
+package com.app.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils {
+	
+	private static final  String URL = "jdbc:mysql://localhost:3306/jee";
+	private static final String DB_USERNAME = "root";
+	private static final String DB_PASSWORD = "Srinivas@192004";
+	private static Connection con;
+	
+	public static  Connection myConnect() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(URL,DB_USERNAME,DB_PASSWORD);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+}
