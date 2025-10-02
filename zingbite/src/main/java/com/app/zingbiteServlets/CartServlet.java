0f0cfc3455c65299155d8e0b56d963498e5a3f48
Sreenivasulu <sreenivasdasariv2004@gmail.com>
2025-10-02 07:39:27 +0530

    cart servlet committed

---
+++ zingbite/src/main/java/com/app/zingbiteServlets/CartServlet.java
@@ -0,0 +1,33 @@
+package com.app.zingbiteServlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.app.zingbitemodels.Cart;
+
+
+@WebServlet("/cart")
+public class CartServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		
+		Cart cart = (Cart)session.getAttribute("cart");
+		
+		if(cart==null) {
+			 cart = new Cart();
+			 session.setAttribute("cart", cart);
+		}
+		
+		String action = req.getParameter("actions");
+	}
+	
+	
+
+}
