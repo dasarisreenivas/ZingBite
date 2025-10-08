baf4df2742a54b9d5fb380c929dd9159741786c3
Sreenivasulu <sreenivasdasariv2004@gmail.com>
2025-10-08 21:38:45 +0530

    updated cartservlet

--- zingbite/src/main/java/com/app/zingbiteServlets/CartServlet.java
+++ zingbite/src/main/java/com/app/zingbiteServlets/CartServlet.java
@@ -16,82 +16,114 @@ import com.app.zingbitemodels.Cart;
 import com.app.zingbitemodels.CartItem;
 import com.app.zingbitemodels.Menu;
 
-
 @WebServlet("/cart")
 public class CartServlet extends HttpServlet {
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		HttpSession session = req.getSession();
-		
-		Cart cart = (Cart)session.getAttribute("cart");
-		
-		if(cart==null) {
-			 cart = new Cart();
-			 session.setAttribute("cart", cart);
-		}
-		
-		String action = req.getParameter("action");
-		if("add".equals(action)) {
-			addItemToCart(req,cart);
-		}
-		else if("update".equals(action)) {
-			updateCartItem(req,cart);
-		}
-		else if("remove".equals(action)) {
-			removeItemFromCart(req,cart);
-		}
-		
-		session.setAttribute("cart", cart);
-		resp.sendRedirect("cart.jsp");
-		
-	}
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("cart.jsp").forward(req, resp);
-	}
-	
-	private void addItemToCart(HttpServletRequest req, Cart cart) {
-		int itemID = Integer.parseInt(req.getParameter("itemId"));
-		int quantity = Integer.parseInt(req.getParameter("quantity"));
-		
-		MenuDAO menuDAO = new MenuDAOImplementation();
-		Menu menuItem = menuDAO.getMenuById(itemID);
-		
-		HttpSession session = req.getSession();
-		int restaurantID = menuItem.getRestaurantId();
-		session.setAttribute("restaurantId", restaurantID);
-		session.setAttribute("restaurantName", new RestaurantDAOImplementation().getRestaurantById(restaurantID).getRestaurantName());
-		if(menuItem!=null) {
-			CartItem item = new CartItem(
-						menuItem.getMenuId(),
-						menuItem.getMenuName(),
-						menuItem.getPrice(),
-						quantity,
-						0.0f
-						
-					);
-			cart.addItemToCart(item);
-		}
-		
-	}
-	
-	private void removeItemFromCart(HttpServletRequest req, Cart cart) {
-		int itemID = Integer.parseInt(req.getParameter("itemId"));
-		cart.removeItemFromCart(itemID);
-		
-	}
 
-	private void updateCartItem(HttpServletRequest req, Cart cart) {
-		int itemID = Integer.parseInt(req.getParameter("itemId"));
-		int quantity = Integer.parseInt(req.getParameter("quantity"));
-		cart.updateCartItem(itemID, quantity);
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Cart cart = (Cart) session.getAttribute("cart");
 
-	
+        if (cart == null) {
+            cart = new Cart();
+            session.setAttribute("cart", cart);
+        }
 
+        String action = req.getParameter("action");
 
-	
-	
+        try {
+            switch (action) {
+                case "add":
+                    addItemToCart(req, cart);
+                    break;
 
+                case "update":
+                    updateCartItem(req, cart);
+                    break;
+
+                case "increase":
+                    adjustQuantity(req, cart, 1);
+                    break;
+
+                case "decrease":
+                    adjustQuantity(req, cart, -1);
+                    break;
+
+                case "remove":
+                    removeItemFromCart(req, cart);
+                    break;
+
+                case "clear":
+                    cart.clear();
+                    break;
+            }
+
+            session.setAttribute("cart", cart);
+
+            // If AJAX request, send OK status without redirect
+            String xhrHeader = req.getHeader("X-Requested-With");
+            if ("XMLHttpRequest".equals(xhrHeader)) {
+                resp.setStatus(HttpServletResponse.SC_OK);
+            } else {
+                resp.sendRedirect("cart.jsp");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Cart operation failed");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("cart.jsp").forward(req, resp);
+    }
+
+    private void addItemToCart(HttpServletRequest req, Cart cart) {
+        int itemID = Integer.parseInt(req.getParameter("itemId"));
+        int quantity = Integer.parseInt(req.getParameter("quantity"));
+
+        MenuDAO menuDAO = new MenuDAOImplementation();
+        Menu menuItem = menuDAO.getMenuById(itemID);
+        HttpSession session = req.getSession();
+        int restaurantId = menuItem.getRestaurantId();
+        session.setAttribute("restaurantId", restaurantId);
+        session.setAttribute("restaurantName",new RestaurantDAOImplementation().getRestaurantById(restaurantId).getRestaurantName());
+        
+        
+        if (menuItem != null) {
+            CartItem item = new CartItem(
+                    menuItem.getMenuId(),
+                    menuItem.getMenuName(),
+                    menuItem.getPrice(),
+                    quantity,
+                    0.0f
+            );
+            cart.addItemToCart(item);
+        }
+    }
+
+    private void removeItemFromCart(HttpServletRequest req, Cart cart) {
+        int itemID = Integer.parseInt(req.getParameter("itemId"));
+        cart.removeItemFromCart(itemID);
+    }
+
+    private void updateCartItem(HttpServletRequest req, Cart cart) {
+        int itemID = Integer.parseInt(req.getParameter("itemId"));
+        int quantity = Integer.parseInt(req.getParameter("quantity"));
+        cart.updateCartItem(itemID, quantity);
+    }
+
+    private void adjustQuantity(HttpServletRequest req, Cart cart, int delta) {
+        int itemID = Integer.parseInt(req.getParameter("itemId"));
+        CartItem item = cart.getItems().get(itemID);
+        if (item != null) {
+            int newQty = item.getQuantity() + delta;
+            if (newQty <= 0) {
+                cart.removeItemFromCart(itemID);
+            } else {
+                cart.updateCartItem(itemID, newQty);
+            }
+        }
+    }
 }
